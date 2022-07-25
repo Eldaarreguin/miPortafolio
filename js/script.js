4234d48e6483360f13e665766c0b75447c7b2d45
Eldaarreguin <arreguin.elda@hotmail.com>
2022-07-24 22:32:30 -0500

    Enviando apuntes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 "use strict"
 
 console.log("Hola mundo, desde mi portfolio")
+
+function mult(a,b) {
+    let resultado = 0;
+    for (let index =1; index <= b; index++) {
+        resultado = resultado + a;
+    }
+    return resultado
+}
+console.log(mult(6,2));
+
+
+/* multiplicar dos numeros sin usar el simbolo de multiplicar
+respuetas: multiply (2,6)
+output: igual a 12 */
